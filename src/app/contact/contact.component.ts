79ad28678c7b2ea97cccf6c0748f0c4d5ecda19a
Karangu Wairore Lucas <kwairore@gmail.com>
2023-07-12 23:41:43 +0300

    - Push to github

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -9,6 +9,9 @@ import { JsPluginsInitService } from '../core/vendor-utils/js-plugins-init.servi
 })
 export class ContactComponent implements OnInit, AfterViewInit {
 
+
+  private maps_url = 'https://maps.google.com/maps/dir//Dhome+Bar+And+Restaurant+Majengo,+Off+Ganda+Road/@-3.2149992,40.1145496,14z/data=!4m5!4m4!1m0!1m2!1m1!1s0x18158fb02820fe7f:0x2bb0fc4f7a5c00fc';
+
   constructor(
     private _jsPluginsInitService: JsPluginsInitService,
   ) { }
@@ -18,9 +21,20 @@ export class ContactComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     this._jsPluginsInitService.initializePageTransition(),
-    this._jsPluginsInitService.initializeDataBackgroundImage(),
-    this._jsPluginsInitService.initializeDataBackgroundColor(),
-    this._jsPluginsInitService.initializeWowAnimation();
+      this._jsPluginsInitService.initializeDataBackgroundImage(),
+      this._jsPluginsInitService.initializeDataBackgroundColor(),
+      this._jsPluginsInitService.initializeWowAnimation();
+  }
+
+  redirectToMaps() {
+    const win = window.open(this.maps_url, '_blank', 'noopener,noreferrer');
+    if (win) {
+      //Browser has allowed it to be opened
+      win.focus();
+    } else {
+      //Browser has blocked it
+      // alert('Please allow popups for this website');
+    }
   }
 
 }
