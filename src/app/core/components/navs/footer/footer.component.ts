282af51e3cd7583e982b0455ad09bebadcb2980c
Karangu Wairore Lucas <kwairore@gmail.com>
2023-07-13 00:07:00 +0300

    - Push to Github

--- src/app/core/components/navs/footer/footer.component.ts
+++ src/app/core/components/navs/footer/footer.component.ts
@@ -8,6 +8,7 @@ import { JsPluginsInitService } from 'src/app/core/vendor-utils/js-plugins-init.
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FooterComponent implements OnInit, AfterViewInit {
+  private maps_url = 'https://maps.google.com/maps/dir//Dhome+Bar+And+Restaurant+Majengo,+Off+Ganda+Road/@-3.2149992,40.1145496,14z/data=!4m5!4m4!1m0!1m2!1m1!1s0x18158fb02820fe7f:0x2bb0fc4f7a5c00fc';
 
   constructor(
     private _jsPluginsInitService: JsPluginsInitService,
@@ -18,6 +19,17 @@ export class FooterComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     this._jsPluginsInitService.goUp();
+  }
+
+  redirectToMaps() {
+    const win = window.open(this.maps_url, '_blank', 'noopener,noreferrer');
+    if (win) {
+      //Browser has allowed it to be opened
+      win.focus();
+    } else {
+      //Browser has blocked it
+      // alert('Please allow popups for this website');
+    }
   }
 
 }
